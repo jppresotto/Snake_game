5e7e5f55c1c3fd31b879262658b8a232feac5d07
Jppresotto <jppresotto7@gmail.com>
2021-07-30 12:17:01 -0300

    Apple's arrive

--- js/snake-js.js
+++ js/snake-js.js
@@ -6,6 +6,12 @@ snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right";
+let food = {
+    x:Math.floor(Math.random() * 15 + 1) * box,
+    y:Math.floor(Math.random() * 15 + 1) * box    
+}
+
 function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0,0,16 * box, 16 * box);
@@ -14,10 +20,48 @@ function criarBG(){
 function criarSnake(){
     for(i=0; i < snake.length; i++){
         context.fillStyle = "green";
-        context.fillRect(snake[i].x, snake[i].y, box, box);
-               
+        context.fillRect(snake[i].x, snake[i].y, box, box);               
     }
 }
+function drawFood(){
+    context.fillStyle = "blue";
+    context.fillRect(food.x, food.y, box, box);
+}
 
-criarBG();
-criarSnake();
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+function iniciarGame(){
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 * box && direction == "left") snake[0].x = 16 * box;
+    if(snake[0].y < 0 * box && direction == "up") snake[0].y = 16 * box;
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+
+    criarBG();
+    criarSnake();
+    drawFood();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,        
+        y: snakeY
+    }
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(iniciarGame, 100);
